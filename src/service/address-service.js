48df345dcc163c69022d654ab30fee93c2c70572
haris2303 <hrsccf102@gmail.com>
2024-03-07 09:46:48 +0900

    address api

--- src/service/address-service.js
+++ src/service/address-service.js
@@ -5,6 +5,7 @@ import { ResponseError } from "../error/response-error.js";
 import {
     createAddressValidation,
     getAddressValidation,
+    updateAddressValidation,
 } from "../validation/address-validation.js";
 
 const checkContactMustExists = async (user, contactId) => {
@@ -69,7 +70,68 @@ const get = async (user, contactId, addressId) => {
     return address;
 };
 
+const update = async (user, contactId, request) => {
+    contactId = await checkContactMustExists(user, contactId);
+    const address = validate(updateAddressValidation, request);
+
+    const totalAddressInDatabase = await prismaClient.address.count({
+        where: {
+            contact_id: contactId,
+            id: address.id,
+        },
+    });
+
+    if (totalAddressInDatabase !== 1) {
+        throw new ResponseError(404, "address is not found");
+    }
+
+    return prismaClient.address.update({
+        where: {
+            id: address.id,
+        },
+        data: {
+            street: address.street,
+            city: address.city,
+            province: address.province,
+            country: address.country,
+            postal_code: address.postal_code,
+        },
+        select: {
+            id: true,
+            street: true,
+            city: true,
+            province: true,
+            country: true,
+            postal_code: true,
+        },
+    });
+};
+
+const remove = async (user, contactId, addressId) => {
+    contactId = await checkContactMustExists(user, contactId);
+    addressId = validate(getAddressValidation, addressId);
+
+    const totalAddressInDatabase = await prismaClient.address.count({
+        where: {
+            contact_id: contactId,
+            id: addressId,
+        },
+    });
+
+    if (totalAddressInDatabase !== 1) {
+        throw new ResponseError(404, "address is not found");
+    }
+
+    return prismaClient.address.delete({
+        where: {
+            id: addressId,
+        },
+    });
+};
+
 export default {
     create,
     get,
+    update,
+    remove,
 };
--- test/address.test.js
+++ test/address.test.js
@@ -148,3 +148,181 @@ describe("GET /api/contacts/:contactId/addresses/:addressId", () => {
         expect(result.status).toBe(404);
     });
 });
+
+describe("PUT /api/contacts/:contactId/addresses/:addressId", () => {
+    beforeEach(async () => {
+        await createTestUser();
+        await createTestContact();
+        await createTestAddress();
+    });
+
+    afterEach(async () => {
+        await removeAllTestAddresses();
+        await removeAllTestContact();
+        await removeTestUser();
+    });
+
+    it("should can update address", async () => {
+        const testContact = await getTestContact();
+        const testAddress = await getTestAddress();
+
+        const result = await supertest(web)
+            .put(
+                "/api/contacts/" +
+                    testContact.id +
+                    "/addresses/" +
+                    testAddress.id
+            )
+            .set("Authorization", "token123")
+            .send({
+                street: "Jalan Durian",
+                city: "Kota Durian",
+                province: "Province Durian",
+                country: "Indo",
+                postal_code: "12345",
+            });
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.id).toBeDefined();
+        expect(result.body.data.street).toBe("Jalan Durian");
+        expect(result.body.data.city).toBe("Kota Durian");
+        expect(result.body.data.province).toBe("Province Durian");
+        expect(result.body.data.country).toBe("Indo");
+        expect(result.body.data.postal_code).toBe("12345");
+    });
+
+    it("should reject if request is not valid", async () => {
+        const testContact = await getTestContact();
+        const testAddress = await getTestAddress();
+
+        const result = await supertest(web)
+            .put(
+                "/api/contacts/" +
+                    testContact.id +
+                    "/addresses/" +
+                    testAddress.id
+            )
+            .set("Authorization", "token123")
+            .send({
+                street: "Jalan Durian",
+                city: "Kota Durian",
+                province: "Province Durian",
+                country: "",
+                postal_code: "",
+            });
+
+        expect(result.status).toBe(400);
+    });
+
+    it("should reject if request address is not found", async () => {
+        const testContact = await getTestContact();
+        const testAddress = await getTestAddress();
+
+        const result = await supertest(web)
+            .put(
+                "/api/contacts/" +
+                    testContact.id +
+                    "/addresses/" +
+                    (testAddress.id + 1)
+            )
+            .set("Authorization", "token123")
+            .send({
+                street: "Jalan Durian",
+                city: "Kota Durian",
+                province: "Province Durian",
+                country: "asd",
+                postal_code: "ads",
+            });
+
+        expect(result.status).toBe(404);
+    });
+
+    it("should reject if request contact is not found", async () => {
+        const testContact = await getTestContact();
+        const testAddress = await getTestAddress();
+
+        const result = await supertest(web)
+            .put(
+                "/api/contacts/" +
+                    (testContact.id + 1) +
+                    "/addresses/" +
+                    testAddress.id
+            )
+            .set("Authorization", "token123")
+            .send({
+                street: "Jalan Durian",
+                city: "Kota Durian",
+                province: "Province Durian",
+                country: "asd",
+                postal_code: "ads",
+            });
+
+        expect(result.status).toBe(404);
+    });
+});
+
+describe("DELETE /api/contacts/:contactId/addresses/:addressId", () => {
+    beforeEach(async () => {
+        await createTestUser();
+        await createTestContact();
+        await createTestAddress();
+    });
+
+    afterEach(async () => {
+        await removeAllTestAddresses();
+        await removeAllTestContact();
+        await removeTestUser();
+    });
+
+    it("should can remove address", async () => {
+        const testContact = await getTestContact();
+        let testAddress = await getTestAddress();
+
+        const result = await supertest(web)
+            .delete(
+                "/api/contacts/" +
+                    testContact.id +
+                    "/addresses/" +
+                    testAddress.id
+            )
+            .set("Authorization", "token123");
+
+        expect(result.status).toBe(200);
+        expect(result.body.data).toBe("OK");
+
+        testAddress = await getTestAddress();
+        expect(testAddress).toBeNull();
+    });
+
+    it("should reject if address is not found", async () => {
+        const testContact = await getTestContact();
+        let testAddress = await getTestAddress();
+
+        const result = await supertest(web)
+            .delete(
+                "/api/contacts/" +
+                    testContact.id +
+                    "/addresses/" +
+                    (testAddress.id + 1)
+            )
+            .set("Authorization", "token123");
+
+        expect(result.status).toBe(404);
+    });
+
+    it("should reject if contact is not found", async () => {
+        const testContact = await getTestContact();
+        let testAddress = await getTestAddress();
+
+        const result = await supertest(web)
+            .delete(
+                "/api/contacts/" +
+                    (testContact.id + 1) +
+                    "/addresses/" +
+                    testAddress.id
+            )
+            .set("Authorization", "token123");
+
+        expect(result.status).toBe(404);
+    });
+});
